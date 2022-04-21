b262deb2365ccb3263bfeac981c6ed078a702754
Nick Thorn <61264673+nickthorn@users.noreply.github.com>
2022-04-21 14:32:06 +1000

    Form Setup

--- app.js
+++ app.js
@@ -7,3 +7,19 @@ function closeNav() {
   document.querySelector('.section-banner__sidebar').style.width = '0';
   document.querySelector('body').style.overflow = 'scroll';
 }
+document
+  .querySelector("form")
+  .addEventListener("submit", handleSubmit);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  let myForm = document.getElementById("enquiry");
+  let formData = new FormData(myForm);
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => alert(error));
+};
